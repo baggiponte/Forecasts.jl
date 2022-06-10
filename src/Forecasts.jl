6073f5f16a912890e79c9481c43c745908b3fedd
baggiponte <lucabaggi.uni@gmail.com>
2022-06-10 23:55:38 +0200

    added first draft of Forecasts main module

--- src/Forecasts.jl
+++ src/Forecasts.jl
@@ -1,1 +1,14 @@
 module Forecasts
+
+# include("timeseries.jl")
+# using .TimeSeriesArrays
+# export TimeSeries
+
+# include("estimators.jl")
+# using .Estimators
+# export NaiveDrift, NaiveSeasonal
+
+include("metrics.jl")
+export RegressionMetrics.mean_absolute_error, RegressionMetrics.mean_squared_error
+
+end

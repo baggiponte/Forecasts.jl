e54426269ce9b550da6e6a860250d534c2b1ae42
baggiponte <lucabaggi.uni@gmail.com>
2022-06-10 23:55:25 +0200

    added docstring to timeseries

--- src/timeseries.jl
+++ src/timeseries.jl
@@ -1,7 +1,22 @@
 module TimeSeriesArrays
 
+using Dates
+
 abstract type AbstractTimeSeries{T,N,D} end
 
+"""
+    TimeArray{T,N,D<:TimeType,A<:AbstractArray{T,N}} <: AbstractTimeSeries{T,N,D}
+
+Creates a TimeSeries with an index (either a Date/DateTime or an Int32) and an Array of values.
+
+# Constructors
+
+# Arguments
+
+- `index::Vector{<:TimeType}`: a vector of sorted timestamps,
+- `values::AbstractArray`: a data vector or matrix. Its number of rows should match the length of `timestamp`.
+- `colnames::Vector{Symbol}`: the column names. Its length should match the column of `values`.
+"""
 struct TimeSeries{T,N,D<:TimeType,A<:AbstractArray{T,N}} <: AbstractTimeSeries{T,N,D}
   index::Vector{D}
   values::A

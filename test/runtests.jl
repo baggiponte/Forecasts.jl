bf0603292d948fcdfd6b58d4e0514603ac4c5a72
baggiponte <lucabaggi.uni@gmail.com>
2022-06-10 23:55:46 +0200

    added first batch of tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,19 @@ using Forecasts
 using Test
 
 @testset "Forecasts.jl" begin
-    # Write your tests here.
+
+    @testset "Mean Absolute Error" begin
+        @test_throws DimensionMismatch mean_absolute_error([0, 0], [1, 1, 1])
+        @test_throws DimensionMismatch mean_absolute_error([0, 0, 0], [1, 1])
+
+        @test mean_absolute_error([0, 0, 0], [1, 1, 1]) == 1
+    end
+
+    @testset "Mean Squared Error" begin
+        @test_throws DimensionMismatch mean_squared_error([0, 0], [1, 1, 1])
+        @test_throws DimensionMismatch mean_squared_error([0, 0, 0], [1, 1])
+
+        @test mean_squared_error([0, 0, 0], [1, 1, 1]) == 1
+        @test mean_squared_error([0, 0, 0], [1, 1, 1], square=true) == 1
+    end
 end

bcd719e932031b9327d198f7025d79b564790cde
baggiponte <lucabaggi.uni@gmail.com>
2022-06-11 12:33:37 +0200

    new exceptions module

---
+++ src/exceptions.jl
@@ -0,0 +1,11 @@
+module ForecastsExceptions
+
+export WindowLengthMismatch
+
+"""
+Error thrown when the window length of an estimator is greater than the length of the vector `y`
+"""
+struct WindowLengthMismatch <: Exception end
+
+
+end
